f3e1aceae04f352f5e0aa2b06c09fe4f01b37fbf
Naimur <naeemhasan28@gmail.com>
2022-08-21 02:14:33 +0600

    first commit ✌️

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tutorial link: https://www.youtube.com/watch?v=HgvIox6ehkM
+
+$(document).ready(() => {
+  $('#readyDemo').text('Document is ready').css('color', 'teal').css('font-size', '20px')
+})
