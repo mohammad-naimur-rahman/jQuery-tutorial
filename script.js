a6a1f8a1dce5c7d22390d1dcbda4ab195718e28e
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-08-23 23:17:22 +0600

    added selectors and events

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
-  //-- selectors
-  $('p').hide() // selects all the p tag - display: hidden
+  //-- Selectors
+  $('pre').hide() // selects all the p tag - display: hidden
   $('#id-name').css('display', 'none')
   $('.class-name').css('display', 'none')
   // $('*') - select everything
@@ -10,4 +10,39 @@ $(document).ready(() => {
   // $('ul li:first') - nested selector
   // $("a[target='_blank']") - attribute selector
   // for more info - https://www.w3schools.com/jquery/jquery_ref_selectors.asp
+
+  //-- Events
+  $('.btn-click').click(() => {
+    $('.btn-next').text('I am popped in!')
+  })
+
+  $('.btn-hide').dblclick(function () {
+    $(this).hide() // fat arrow funciton mix up 'this' keyword, that's why I used old style function here
+  })
+
+  let countEnter = 0,
+    countLeave = 0
+
+  // $('.counter-box').mouseenter(function () {
+  //   countEnter += 1
+  //   $('.counter-enter').text(countEnter)
+  // })
+
+  // $('.counter-box').mouseleave(function () {
+  //   countLeave += 1
+  //   $('.counter-leave').text(countLeave)
+  // })
+
+  // Alternative way - for multiple events on one element
+  $('.counter-box').on({
+    mouseenter: function () {
+      countEnter += 1
+      $('.counter-enter').text(countEnter)
+    },
+    mouseleave: function () {
+      countLeave += 1
+      $('.counter-leave').text(countLeave)
+    },
+  })
+  // For more info - https://www.w3schools.com/jquery/jquery_ref_events.asp
 })
