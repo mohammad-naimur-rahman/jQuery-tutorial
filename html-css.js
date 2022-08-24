18c4354d738520f4031147c544b424f641f04a9d
Naimur <naeemhasan28@gmail.com>
2022-08-25 03:02:37 +0600

    added set with callback

--- html-css.js
+++ html-css.js
@@ -14,5 +14,27 @@ $(document).ready(function () {
   $('.set-html').html(`
     <span class="text-primary">This is set custom HTML</span>
   `) // set innerHTML
+  $('.set-value').val('Some value is input') // set value
   $('.set-attr').attr('href', 'index.html') // set attribute value
+
+  // set with callback
+  $('.set-text-cb').text(function (i, old) {
+    return old + ' and this is the new text and the index is ' + i
+  })
+  $('.set-html-cb').html(function (i, old) {
+    return old + '<span class="text-primary"> and this is the new html and the index is</span> ' + i
+  })
+  $('.set-attr-cb').attr('href', function (i, old) {
+    return old + '/index.html'
+  })
+  $('.inc').click(function () {
+    $('.set-value-cb').val(function (i, old) {
+      return +old + 1
+    })
+  })
+  $('.dec').click(function () {
+    $('.set-value-cb').val(function (i, old) {
+      return +old - 1
+    })
+  })
 })
