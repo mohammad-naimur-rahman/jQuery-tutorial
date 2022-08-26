cf147213074d4a49eccf135394a3ddb1c490541a
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-08-26 20:37:58 +0600

    will be back on this section later

--- js/traversing.js
+++ js/traversing.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   //-- Ancestors
-  // parent()
-  $('.child').parent()
+  $('.find-parent').parent().css('border', '1px solid orangered') // parent()
+  $('.find-parents').parents().css('border', '1px solid orangered') // parents()
+  $('.find-parents-until').parentsUntil('.find-until').css('border', '1px solid orangered') // parentsUntil()
 })
